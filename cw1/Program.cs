805b50991bafc096565c0be7cc87322a889fdfaf
Michal Razowski <s13812@pjwstk.edu.pl>
2020-10-24 14:26:43 +0100

    Zadanie 5

--- cw1/Program.cs
+++ cw1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,8 +11,28 @@ namespace cw1
     {
         public static async Task Main(string[] args)
         {
+            var url = "";
+
+            try
+            {
+                url = args[0];
+            }
+            catch
+            {
+                throw new ArgumentNullException("arg[0]");
+            }            
+
+            var urlRegex = new Regex("^(https?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+
+            if (!urlRegex.IsMatch(url))
+            {
+                throw new ArgumentException("arg[0] nie jest poprawnym adresem URL");
+            }
+
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(args[0]);
+            var response = await httpClient.GetAsync(url);
+
+            httpClient.Dispose();
 
             if (response.IsSuccessStatusCode) 
             {
@@ -18,13 +40,24 @@ namespace cw1
                 var regex = new Regex("[a-z0-9]+@[a-z.]+");
 
                 var matches = regex.Matches(html);
-                foreach(var i in matches)
+
+                if (matches.Count == 0)
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine("Nie znaleziono adresow email");
+                } else
+                {
+                    foreach (var i in (from Match m in matches select m.Value).Distinct())
+                    {
+                        Console.WriteLine(i);
+                    }
                 }
+
+            } else
+            {
+                Console.WriteLine("Blad w czasie pobierania strony");
             }
 
-            Console.WriteLine("Koniec!");
+            Console.WriteLine("\nKoniec!");
         }
     }
 }
